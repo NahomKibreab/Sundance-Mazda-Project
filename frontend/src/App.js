60b3111048082c1da0eec35be9ad19e3e81ca5a3
NahomKibreab <nknahom@gmail.com>
2021-09-26 01:37:16 -0600

    Connect fontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get("/db").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  const names = data && data.map((d) => <div key={d.id}>{d.name}</div>);
+  return <div className="App">{names}</div>;
 }
 
 export default App;
