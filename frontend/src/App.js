6d83385114eb3f729061bb2e02994cdfcf4338b7
Amit Baid <baidstar@hotmail.com>
2021-09-26 14:51:19 -0600

    add react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Grid } from "@mui/material";
 import NavBar from "./components/NavBar";
 import VehicleCard from "./components/VehicleCard";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   // const [data, setData] = useState();
@@ -19,28 +20,47 @@ function App() {
     </Grid>
   ));
   return (
-    <div className="App">
-      <NavBar />
-      <Grid container>
-        <Grid item xs={12} sm={2} sx={{ background: "red" }}>
-          Left
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Grid container>
+          <Grid item xs={12} sm={2} sx={{ background: "red" }}>
+            Left
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            container
+            spacing={2}
+            justifyContent={"center"}
+            sx={{ paddingTop: "16px" }}
+          >
+            <Switch>
+              <Route path="/cars/1">
+                <Link to="/">Home</Link>
+                <VehicleCard />
+              </Route>
+              <Route path="/cars">
+                <Link to="/">Home</Link>
+                <Link to="/cars/1">Car 1</Link>
+                {cars}
+              </Route>
+
+              <Route path="/">
+                <Link to="/cars">Cars</Link>
+              </Route>
+            </Switch>
+          </Grid>
+
+          <Grid item xs={12} sm={2} sx={{ background: "red" }}>
+            Right
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          container
-          spacing={2}
-          justifyContent={"center"}
-          sx={{ paddingTop: "16px" }}
-        >
-          {cars}
-        </Grid>
-        <Grid item xs={12} sm={2} sx={{ background: "red" }}>
-          Right
-        </Grid>
-      </Grid>
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      </div>
+    </Router>
   );
 }
 
