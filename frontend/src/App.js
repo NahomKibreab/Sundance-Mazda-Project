6a52c9f68185c33d0e687824e6d4b758f138908f
Amit Baid <baidstar@hotmail.com>
2021-09-28 10:14:51 -0600

    remove duplicate code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,44 +3,13 @@ import { Grid } from "@mui/material";
 import NavBar from "./components/NavBar";
 import VehicleCard from "./components/VehicleCard";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import StripeCheckout from "react-stripe-checkout";
-import useStripe from "./hooks/useStripe";
+// import StripeCheckout from "react-stripe-checkout";
+// import useStripe from "./hooks/useStripe";
 // import { useState } from "react";
 // import axios from "axios";
 
 function App() {
-  // const [data, setData] = useState();
-  const { product, setProduct, makePayment } = useStripe();
-
-  // useEffect(() => {
-  //   axios.get("/db").then((res) => {
-  //     setData(res.data);
-  //   });
-  // }, []);
-
-  // const names = data && data.map((d) => <div key={d.id}>{d.name}</div>);
-
-  // const [product, setProduct] = useState({
-  //   name: "React from FB",
-  //   price: 10,
-  //   productBy: "facebook",
-  // });
-
-  // const makePayment = (token) => {
-  //   const body = {
-  //     token,
-  //     product,
-  //   };
-
-  //   return axios
-  //     .post("http://localhost:8000/payment", { ...body })
-  //     .then((response) => {
-  //       console.log("Response", response);
-  //       const { status } = response;
-  //       console.log("Status", status);
-  //     })
-  //     .catch((error) => console.log(error));
-  // };
+  // const { product, setProduct, makePayment } = useStripe();
 
   const cars = [1, 2, 3, 4, 5, 6, 7].map((car) => (
     <Grid item>
@@ -65,12 +34,12 @@ function App() {
             justifyContent={"center"}
             sx={{ paddingTop: "16px" }}
           >
-            <StripeCheckout
+            {/* <StripeCheckout
               stripeKey={process.env.REACT_APP_STRIPE_SKEY}
               token={makePayment}
               name="Vehicle Purchase"
               amount={product.price * 100}
-            />
+            /> */}
             <Switch>
               <Route path="/cars/1">
                 <Link to="/">Home</Link>
