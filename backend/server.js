a5bbddede124094376ca493adcef81b6a5712ad5
NahomKibreab <nknahom@gmail.com>
2021-09-27 21:38:41 -0600

    Add stripe payment endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,13 @@ const app = express();
 const PORT = process.env.EXPRESSPORT || 8000;
 const morgan = require("morgan");
 const db = require("./db");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const stripe = require("stripe")(process.env.STRIPE_SKEY);
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(`Server is listening on port ${PORT}`);
@@ -20,6 +25,32 @@ app.get("/db", (req, res) => {
   ).then((data) => {
     res.json(data.rows);
   });
+});
+
+// Stripe payment endpoint
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("Product", product);
+  console.log("Price", product.price);
+  const idempotencyKey = uuidv4();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
