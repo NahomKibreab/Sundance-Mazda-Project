19a579e9d30623b1e4216e03b88d561f94026608
NahomKibreab <nknahom@gmail.com>
2021-09-30 16:37:44 -0600

    Update vehicles response data

--- backend/routes/vehicles.js
+++ backend/routes/vehicles.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 module.exports = (db) => {
   router.get("/cars", (req, res) => {
     db.query(`SELECT * from vehicles;`).then((data) => {
-      res.send(data);
+      res.send(data.rows);
     });
   });
 
