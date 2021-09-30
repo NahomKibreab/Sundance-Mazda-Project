95440875687ea218e67ec9b950c84686788debf2
NahomKibreab <nknahom@gmail.com>
2021-09-29 22:30:34 -0600

    Modify HomeMap canvas size

--- frontend/src/components/HomeMap.js
+++ frontend/src/components/HomeMap.js
@@ -3,6 +3,7 @@ import GoogleMapReact from "google-map-react";
 import "../styles/map.css";
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker";
+import { Typography } from "@mui/material";
 const LocationPin = ({ text }) => (
   <div className="pin">
     <Icon icon={locationIcon} className="pin-icon" />
@@ -12,7 +13,9 @@ const LocationPin = ({ text }) => (
 
 const Map = ({ location, zoomLevel }) => (
   <div className="map" style={{ width: "100%" }}>
-    <h2 className="map-h2">Come Visit Us!</h2>
+    <Typography variant="h5" p={4}>
+      Come Visit Us!
+    </Typography>
 
     <div className="google-map">
       <GoogleMapReact
