b585445f7e0ea2ef4a44b9ca494ebc4366e9ee2c
NahomKibreab <nknahom@gmail.com>
2021-10-02 00:10:53 -0600

    Add sold car route

--- backend/routes/vehicles.js
+++ backend/routes/vehicles.js
@@ -10,11 +10,11 @@ module.exports = (db) => {
 
   // change car unavaliable / sold
   router.put("/cars/:car_id", (req, res) => {
-    console.log("car_id", req.params.car_id);
-    res.send(req.params.car_id);
-    // db.query(`SELECT * from vehicles WHERE avaliable=true;`).then((data) => {
-    //   res.send(data.rows);
-    // });
+    db.query(`UPDATE vehicles set avaliable=false WHERE id=$1;`, [
+      req.params.car_id,
+    ]).then((data) => {
+      res.send(data.rows);
+    });
   });
 
   return router;
