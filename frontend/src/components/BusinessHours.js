2db143b9492620d3dd3658d7a358d7ab0f395fab
Amit Baid <baidstar@hotmail.com>
2021-10-02 12:41:10 -0600

    create business hours tabs component. update footer

---
+++ frontend/src/components/BusinessHours.js
@@ -0,0 +1,162 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BusinessHours() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="Business Hours"
+          centered
+          textColor="#000"
+          borderColor="#000"
+        >
+          <Tab label="Sales" {...a11yProps(0)} />
+          <Tab label="Service" {...a11yProps(1)} />
+          <Tab label="Parts" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0} alignContent="center">
+        <table>
+          <tr>
+            <td>Monday</td>
+            <td>9:00AM - 8:30PM</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>9:00AM - 8:30PM</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>9:00AM - 8:30PM</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>9:00AM - 8:30PM</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>9:00AM - 6:00PM</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>9:00AM - 6:00PM</td>
+          </tr>
+          <tr>
+            <td>Sunday</td>
+            <td>12:00PM - 5:00PM</td>
+          </tr>
+        </table>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <table>
+          <tr>
+            <td>Monday</td>
+            <td>7:00AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>7:00AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>7:00AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>7:00AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>7:00AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>8:00AM - 4:00PM</td>
+          </tr>
+          <tr>
+            <td>Sunday</td>
+            <td>Closed</td>
+          </tr>
+        </table>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <table>
+          <tr>
+            <td>Monday</td>
+            <td>7:30AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>7:30AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>7:30AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>7:30AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>7:30AM - 5:00PM</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>8:00AM - 4:00PM</td>
+          </tr>
+          <tr>
+            <td>Sunday</td>
+            <td>Closed</td>
+          </tr>
+        </table>
+      </TabPanel>
+    </Box>
+  );
+}
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,55 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Box,
+  Grid,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import BusinessHours from "./BusinessHours";
 
 export default function Footer() {
   return (
-    <AppBar position="static" color="primary" sx={{ marginTop: "24px" }}>
-      <Container maxWidth="md">
-        <Toolbar>
-          <Typography variant="body1" color="inherit">
-            © 2019 Gistia
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Box sx={{ flexGrow: 1, marginBottom: "64px" }}>
+      <AppBar position="static" color="primary" sx={{ marginTop: "24px" }}>
+        <Grid container justifyContent="center" alignItems="center" spacing={1}>
+          <Grid item xs={12} sm={6} justifyContent="center" alignItems="center">
+            <Typography variant="h6" color="inherit">
+              Contact Us
+              <br></br>
+              <br></br>
+            </Typography>
+            <Typography variant="h7" fontWeight="bold" color="inherit">
+              Sundance Mazda
+              <br></br>
+            </Typography>
+            <Typography variant="h7" fontWeight="light" color="inherit">
+              <Link
+                to="https://www.google.com/maps?q=Sundance+Mazda+17990+102+Ave+NW+T5S+1M9+AB+Edmonton"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                17990 102 Ave NW,
+                <br></br>
+                Edmonton, Alberta, T5S 1M9
+                <br></br>
+                <br></br>
+              </Link>
+              Sales: 1-844-392-3633
+              <br></br>
+              Service: 1-844-472-8053
+              <br></br>
+              Parts: 1-780-454-7278
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h6" color="inherit">
+              Business Hours
+              <BusinessHours />
+            </Typography>
+          </Grid>
+        </Grid>
+      </AppBar>
+    </Box>
   );
 }
