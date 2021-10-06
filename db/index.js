2173a4379d20443314c5833413924b6c984c06a7
NahomKibreab <nknahom@gmail.com>
2021-10-06 14:27:28 -0600

    Configure Database Connection

--- db/index.js
+++ db/index.js
@@ -2,13 +2,22 @@
 require("dotenv").config();
 
 const { Pool } = require("pg");
-const pool = new Pool({
+
+const devConfig = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-});
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 pool
   .connect()
